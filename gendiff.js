e89e16c78faf11771c7f77b8361e6d2e7e1fb617
BAIRAPSHI <bairapshi@mail.ru>
2024-05-03 14:07:29 +0300

    mod gendiff.js file

--- gendiff.js
+++ gendiff.js
@@ -6,8 +6,12 @@ const program = new Command();
 program
   .version('1.0.0', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<file1> <file2>')
-  .action((file1, file2) => {
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2, options) => {
+    console.log('Filepath 1:', filepath1);
+    console.log('Filepath 2:', filepath2);
+    console.log('Format:', options.format || 'default');
     console.log('Display diff logic goes here');
   });
 
