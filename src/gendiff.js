670ab20dcf5a2a2bd4640904455634ff9e8b0f6a
BAIRAPSHI <bairapshi@mail.ru>
2024-05-04 23:10:22 +0300

    parsing

---
+++ src/gendiff.js
@@ -0,0 +1,19 @@
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filePath1> <filePath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filePath1, filePath2, options) => {
+    console.log('Filepath 1:', filePath1);
+    console.log('Filepath 2:', filePath2);
+    console.log('Format:', options.format || 'default');
+    console.log('Display diff logic goes here');
+  });
+
+program.parse(process.argv);
+
+
