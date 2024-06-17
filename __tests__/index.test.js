73beb9b1cdd2f6534d9590f3dd504ca02b744130
Баира Пшихачева <bairapsihaceva@Noutbuk-Baira.local>
2024-06-17 17:41:49 +0300

    check json

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,38 +3,29 @@ import yaml from 'yaml';
 import genDiff from '../src/index.js';
 import {
   filePath1, filePath2, fileYaml1, fileYaml2, filePath3, filePath4, fileYaml3, fileYaml4,
-}
-  from '../parse/path.js';
-
-let expectedDiffContent;
-let expectedDiffContent2;
-let expectedFlatContent;
-let jsonResult;
-
-beforeEach(() => {
-  expectedDiffContent = fs.readFileSync('__fixtured__/file1.txt', 'utf-8').replace(/\r?\n|\r/g, '');
-  expectedDiffContent2 = fs.readFileSync('__fixtured__/file2.txt', 'utf-8');
-  expectedFlatContent = fs.readFileSync('__fixtured__/file3.txt', 'utf-8');
-  jsonResult = fs.readFileSync('__fixtured__/file4.txt', 'utf-8');
-});
+} from '../parse/path.js';
 
 describe('gendiff', () => {
   test('should return a comparison of flat files JSON', () => {
+    const expectedDiffContent = fs.readFileSync('__fixtured__/file1.txt', 'utf-8').replace(/\r?\n|\r/g, '');
     const resultDiffJson = genDiff(filePath1, filePath2).replace(/\r?\n|\r/g, '');
     expect(resultDiffJson).toEqual(expectedDiffContent);
   });
 
   test('should return a comparison of flat files yaml', () => {
+    const expectedDiffContent = fs.readFileSync('__fixtured__/file1.txt', 'utf-8').replace(/\r?\n|\r/g, '');
     const resultDiffYaml = yaml.stringify(genDiff(fileYaml1, fileYaml2).replace(/\r?\n|\r/g, ''));
     expect(resultDiffYaml).toEqual(yaml.stringify(expectedDiffContent));
   });
 
   test('should return a comparison of nested files JSON', () => {
+    const expectedDiffContent2 = fs.readFileSync('__fixtured__/file2.txt', 'utf-8');
     const resultDiffJson2 = genDiff(filePath3, filePath4);
     expect(resultDiffJson2).toEqual(expectedDiffContent2);
   });
 
   test('should return a comparison of nested files yaml', () => {
+    const expectedDiffContent2 = fs.readFileSync('__fixtured__/file2.txt', 'utf-8');
     const resultDiffYaml2 = yaml.stringify(genDiff(fileYaml3, fileYaml4));
     expect(resultDiffYaml2).toEqual(yaml.stringify(expectedDiffContent2));
   });
@@ -42,10 +33,12 @@ describe('gendiff', () => {
 
 describe('formatters', () => {
   test('should return flat format plain', () => {
+    const expectedFlatContent = fs.readFileSync('__fixtured__/file3.txt', 'utf-8');
     expect(genDiff(filePath3, filePath4, 'plain')).toEqual(expectedFlatContent);
   });
 
   test('should return flat format json', () => {
+    const jsonResult = fs.readFileSync('__fixtured__/file4.txt', 'utf-8');
     expect(genDiff(filePath3, filePath4, 'json')).toEqual(jsonResult);
   });
 });
