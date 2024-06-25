1fd5f6bd9b591572051059d18e56d8c9538a2dea
Баира Пшихачева <bairapsihaceva@192.168.1.11>
2024-06-25 10:32:16 +0300

    modification fuction genDiff

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import parse from './parse/parser.js';
 import findDifferences from './formatDiff.js';
 import chooseFormatter from './formatters/index.js';
 
+const getFileExtension = (filename) => {
+  const parts = filename.split('.');
+  return parts[parts.length - 1];
+};
+
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const content1 = fs.readFileSync(filepath1, 'utf-8');
   const content2 = fs.readFileSync(filepath2, 'utf-8');
 
-  const type1 = filepath1.split('.').pop();
-  const type2 = filepath2.split('.').pop();
+  const type1 = getFileExtension(filepath1);
+  const type2 = getFileExtension(filepath2);
 
   const data1 = parse(content1, type1);
   const data2 = parse(content2, type2);
