530687b2d945f1811b7fc781e5e81431277adecd
Баира Пшихачева <bairapsihaceva@192.168.1.11>
2024-06-25 10:20:14 +0300

    adding cangedb status to the comparison function

--- src/parse/parser.js
+++ src/parse/parser.js
@@ -6,6 +6,8 @@ const parse = (data, type) => {
       return JSON.parse(data);
     case 'yaml':
       return yaml.parse(data);
+    default:
+      throw new Error('JSON and YAML formats not found');
   }
 };
 
